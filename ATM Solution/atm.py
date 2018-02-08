ac2475aec7a54659fd24c519a68c9369029ae5f0
Mohammed Abdelaziz <mohammed.ali.abdelaziz@gmail.com>
2018-02-08 10:48:52 +0400

    Add files via upload

---
+++ ATM Solution/atm.py
@@ -0,0 +1,12 @@
+balance = 500
+current = 0
+
+moneyCategroies = [100,100,50,10,10,5,2]
+
+for x in moneyCategroies:
+	balance = balance - x;
+	current = current+x;
+	print "give %r" % x	
+
+print "withdrawn amount is %r" % current
+print "your current balance is %d" % balance
