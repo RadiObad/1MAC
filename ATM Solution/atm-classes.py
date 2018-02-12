a222efcfc0e9b8e45ba43b5f2e9daae4275157aa
Mohammed Abdelaziz <mohammed.ali.abdelaziz@gmail.com>
2018-02-13 02:11:53 +0400

    Update atm-classes.py

--- ATM Solution/atm-classes.py
+++ ATM Solution/atm-classes.py
@@ -10,31 +10,35 @@ class ATM:
     print "Your current balance is: %r" % self.balance
     print "="*50
 
+ def Calculate(self,request):
+    while request > 0:
+        if request >= 100:
+            print "give 100"
+            request = request - 100
+        elif request >= 50:
+            print "give 50"
+            request = request - 50
+        elif request >= 20:
+            print "give 20"
+            request = request - 20
+        elif request >= 10:
+            print "give 10"
+            request = request - 10
+        elif request >= 5:
+            print "give 5"
+            request = request -5
+        else:
+            print ("give " + str(request))
+            request = 0 
+    return request
+
  def WithDraw(self, request):
     if request > self.balance:
         print "can't give all this money"
     elif request <= 0 :
         print "no transaction to continue"    
     else:
-        while request > 0:
-            if request >= 100:
-                print "give 100"
-                request = request - 100
-            elif request >= 50:
-                print "give 50"
-                request = request - 50
-            elif request >= 20:
-                print "give 20"
-                request = request - 20
-            elif request >= 10:
-                print "give 10"
-                request = request - 10
-            elif request >= 5:
-                print "give 5"
-                request = request -5
-            else:
-                print ("give " + str(request))
-                request = 0        
+        request = self.Calculate(request)       
             
     return request
     
